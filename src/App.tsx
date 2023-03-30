6a62e83c2d6e924c81d9cd64943c6b6fcca421c6
Casey <caseydyps@gmail.com>
2023-03-30 10:40:21 +0800

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { createGlobalStyle } from 'styled-components';
+import './App.css';
+
+function App() {
+  return <div className="App">EveryDay</div>;
+}
+
+export default App;
