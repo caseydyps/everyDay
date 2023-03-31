dcac1419cdd78c8340b64b815185973d944eb1b3
Casey <caseydyps@gmail.com>
2023-03-31 18:00:00 +0800

    feature user can delete sticky notes

--- src/Components/drawingtool.js
+++ src/Components/drawingtool.js
@@ -2,6 +2,8 @@ import React, { useState, useRef, useEffect } from 'react';
 
 const DrawingTool = () => {
   const [isPainting, setIsPainting] = useState(false);
+  const [color, setColor] = useState('black');
+  const [width, setWidth] = useState(5);
   const canvasRef = useRef(null);
   const contextRef = useRef(null);
 
@@ -15,10 +17,10 @@ const DrawingTool = () => {
     const context = canvas.getContext('2d');
     context.scale(2, 2);
     context.lineCap = 'round';
-    context.strokeStyle = 'black';
-    context.lineWidth = 5;
+    context.strokeStyle = color;
+    context.lineWidth = width;
     contextRef.current = context;
-  }, []);
+  }, [color, width]);
 
   const startPaint = ({ nativeEvent }) => {
     const { offsetX, offsetY } = nativeEvent;
@@ -41,6 +43,14 @@ const DrawingTool = () => {
     contextRef.current.stroke();
   };
 
+  const handleChangeColor = (newColor) => {
+    setColor(newColor);
+  };
+
+  const handleChangeLineWidth = (newWidth) => {
+    setWidth(newWidth);
+  };
+
   const clearCanvas = () => {
     const canvas = canvasRef.current;
     const context = canvas.getContext('2d');
@@ -48,7 +58,7 @@ const DrawingTool = () => {
   };
 
   return (
-    <>
+    <div>
       <canvas
         id="canvas"
         ref={canvasRef}
@@ -56,8 +66,15 @@ const DrawingTool = () => {
         onMouseUp={endPaint}
         onMouseMove={paint}
       />
+      <button onClick={() => handleChangeColor('black')}>Black</button>
+      <button onClick={() => handleChangeColor('red')}>Red</button>
+      <button onClick={() => handleChangeColor('green')}>Green</button>
+      <button onClick={() => handleChangeColor('blue')}>Blue</button>
+      <button onClick={() => handleChangeLineWidth(5)}>Default</button>
+      <button onClick={() => handleChangeLineWidth(1)}>Thin</button>
+      <button onClick={() => handleChangeLineWidth(30)}>Wide</button>
       <button onClick={clearCanvas}>Clear</button>
-    </>
+    </div>
   );
 };
 
--- src/pages/Dashboard/Whiteboard.tsx
+++ src/pages/Dashboard/Whiteboard.tsx
@@ -13,22 +13,32 @@ const Wrapper = styled.div`
   position: relative;
   width: 100%;
   height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 `;
 
 const Sticker = styled.div<{
   dragging: boolean;
   offsetX: number;
   offsetY: number;
+  color: string;
 }>`
   position: absolute;
   top: ${(props) => props.offsetY}px;
   left: ${(props) => props.offsetX}px;
-  background-color: ${(props) => (props.dragging ? 'lightblue' : 'yellow')};
+  background-color: ${({ color }) => color};
   cursor: ${(props) => (props.dragging ? 'grabbing' : 'grab')};
-  width: 150px;
-  height: 100px;
-  border: 2px solid black;
+  width: 250px;
+  height: 250px;
+  border: 0px solid black;
   border-radius: 5px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 18px;
+  font-weight: bold;
 `;
 
 const AddButton = styled.button`
@@ -41,14 +51,51 @@ const AddButton = styled.button`
   cursor: pointer;
 `;
 
+const ColorButton = styled.button<{ color: string }>`
+  margin-top: 10px;
+  padding: 10px 20px;
+  font-size: 16px;
+  background-color: ${({ color }) => color};
+  border: none;
+  border-radius: 5px;
+  cursor: pointer;
+  color: white;
+  &:hover {
+    opacity: 0.8;
+  }
+`;
+const StickerInput = styled.input`
+  font-size: 16px;
+  border: none;
+
+  outline: none;
+  margin-top: 10px;
+  width: 100%;
+`;
+
+const DeleteButton = styled.button`
+  position: absolute;
+  top: 5px;
+  right: 5px;
+  padding: 5px;
+  font-size: 16px;
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+`;
+
 export const Whiteboard = () => {
   const [stickers, setStickers] = useState<Sticker[]>([]);
+  const [newStickerColor, setNewStickerColor] = useState<string>('yellow');
   const stickerRefs = useRef<(HTMLDivElement | null)[]>([]);
   const [dragging, setDragging] = useState<number | null>(null);
   const [offset, setOffset] = useState<{ x: number; y: number }>({
     x: 0,
     y: 0,
   });
+  const [stickerText, setStickerText] = useState<string[]>(
+    Array(stickers.length).fill('')
+  );
 
   useLayoutEffect(() => {
     const onMouseMove = (e: MouseEvent) => {
@@ -56,7 +103,7 @@ export const Whiteboard = () => {
         const newX = e.clientX - offset.x;
         const newY = e.clientY - offset.y;
         const newStickers = [...stickers];
-        newStickers[dragging] = { x: newX, y: newY };
+        newStickers[dragging] = { ...newStickers[dragging], x: newX, y: newY };
         setStickers(newStickers);
       }
     };
@@ -79,9 +126,7 @@ export const Whiteboard = () => {
     setDragging(id);
     const index = stickers.findIndex((sticker) => sticker.id === id);
     const rect = stickerRefs.current[index]?.getBoundingClientRect();
-    console.log('rect' + rect);
     if (rect) {
-      console.log('here!');
       const offsetX = isNaN(rect.offsetLeft) ? 0 : e.clientX - rect.offsetLeft;
       const offsetY = isNaN(rect.offsetTop) ? 0 : e.clientY - rect.offsetTop;
       setOffset({ x: offsetX, y: offsetY });
@@ -89,29 +134,62 @@ export const Whiteboard = () => {
   };
 
   const addSticker = () => {
-    const newSticker = { id: Date.now(), x: 150, y: 150 };
+    const newSticker = { id: Date.now(), x: 150, y: 150, content: 'New note' };
     setStickers([...stickers, newSticker]);
+    setStickerText([...stickerText, '']);
+  };
+
+  const deleteSticker = (index: number) => {
+    const newStickers = [...stickers];
+    newStickers.splice(index, 1);
+    setStickers(newStickers);
+
+    const newStickerText = [...stickerText];
+    newStickerText.splice(index, 1);
+    setStickerText(newStickerText);
   };
 
   return (
     <Wrapper>
-      {stickers.map((position, index) => (
+      {stickers.map((sticker, index) => (
         <Sticker
-          key={index}
+          key={sticker.id}
+          color={
+            sticker.id === stickers[stickers.length - 1]?.id
+              ? newStickerColor
+              : 'yellow'
+          }
           onMouseDown={(e) => onStickerMouseDown(index, e)}
           ref={(el) => (stickerRefs.current[index] = el)}
           style={{
-            left: position.x - (dragging === index ? offset.x : 0),
-            top: position.y - (dragging === index ? offset.y : 0),
+            left: sticker.x - (dragging === index ? offset.x : 0),
+            top: sticker.y - (dragging === index ? offset.y : 0),
           }}
         >
-          便條紙 {index + 1}
+          <StickerInput
+            type="text"
+            value={stickerText[index]}
+            onChange={(e) => {
+              const newStickerText = [...stickerText];
+              newStickerText[index] = e.target.value;
+              setStickerText(newStickerText);
+            }}
+          />
+          <DeleteButton onClick={() => deleteSticker(index)}>X</DeleteButton>
         </Sticker>
       ))}
       <AddButton onClick={addSticker}>Add Sticker</AddButton>
+      <ColorButton onClick={() => setNewStickerColor('#FFF9C4')}>
+        Yellow
+      </ColorButton>
+      <ColorButton onClick={() => setNewStickerColor('red')}>Red</ColorButton>
+      <ColorButton onClick={() => setNewStickerColor('blue')}>Blue</ColorButton>
+      <ColorButton onClick={() => setNewStickerColor('green')}>
+        Green
+      </ColorButton>
+      <ColorButton onClick={() => setStickers([])}>Clear</ColorButton>
       <DrawingTool />
     </Wrapper>
   );
 };
-
 export default Whiteboard;
