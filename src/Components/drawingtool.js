02dae4eda8b26d015a77f32ec62cc4229bc914fd
Casey <caseydyps@gmail.com>
2023-04-06 13:35:45 +0800

    fix:canva user change color/linewidth won't clear canva

--- src/Components/drawingtool.js
+++ src/Components/drawingtool.js
@@ -23,19 +23,21 @@ const DrawingTool = () => {
     const context = canvas.getContext('2d');
     context.scale(2, 2);
     context.lineCap = 'round';
-    context.strokeStyle = color;
     context.lineWidth = width;
+    context.strokeStyle = color;
     contextRef.current = context;
-  }, [color, width]);
+  }, []);
 
   const startPaint = ({ nativeEvent }) => {
     const { offsetX, offsetY } = nativeEvent;
+    contextRef.current.strokeStyle = color;
     contextRef.current.beginPath();
     contextRef.current.moveTo(offsetX, offsetY);
     setIsPainting(true);
   };
 
   const endPaint = () => {
+    contextRef.current.lineWidth = width;
     contextRef.current.closePath();
     setIsPainting(false);
   };
@@ -65,13 +67,6 @@ const DrawingTool = () => {
 
   return (
     <CanvasWrap>
-      <canvas
-        id="canvas"
-        ref={canvasRef}
-        onMouseDown={startPaint}
-        onMouseUp={endPaint}
-        onMouseMove={paint}
-      />
       <button onClick={() => handleChangeColor('black')}>Black</button>
       <button onClick={() => handleChangeColor('red')}>Red</button>
       <button onClick={() => handleChangeColor('green')}>Green</button>
@@ -80,6 +75,13 @@ const DrawingTool = () => {
       <button onClick={() => handleChangeLineWidth(1)}>Thin</button>
       <button onClick={() => handleChangeLineWidth(30)}>Wide</button>
       <button onClick={clearCanvas}>Clear</button>
+      <canvas
+        id="canvas"
+        ref={canvasRef}
+        onMouseDown={startPaint}
+        onMouseUp={endPaint}
+        onMouseMove={paint}
+      />
     </CanvasWrap>
   );
 };
